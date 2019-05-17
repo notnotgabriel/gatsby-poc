2ec65c26665647636b37e149db4c5b8d6a7b9dca
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2019-05-17 17:46:21 -0300

    pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/header'
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerTitle="About" />
+    <Header headerTitle="Duplicated" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
