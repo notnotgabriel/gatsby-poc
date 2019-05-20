581c108a6413145bcc1651f3d88bc994fe38451d
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2019-05-20 09:34:54 -0300

    acabei esquecendo fazer pequenos commits

---
+++ src/pages/foo.md
@@ -0,0 +1,8 @@
+---
+title: 'FOO pandas'
+date: '2019-05-18'
+---
+
+Foo. Pandas.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,62 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import Header from '../components/header'
+import { graphql } from 'gatsby'
+import { css } from '@emotion/core'
+import { rhythm } from '../utils/typography'
+import Layout from '../layouts/layout'
 
-export default () => (
-  <div style={{ color: `purple` }}>
-    <Header headerTitle="Home" />
-    <Link to="/contact/">Contact</Link>
-    <p>show</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
-)
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <h1
+          css={css`
+            display: inline-block;
+            border-bottom: 1px solid;
+          `}
+        >
+          Amazing Pandas Eating Things
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3
+              css={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+            >
+              {node.frontmatter.title}{' '}
+              <span
+                css={css`
+                  color: #bbb;
+                `}
+              >
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.timeToRead} min read</p>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD/MM/YY")
+          }
+          excerpt
+          timeToRead
+        }
+      }
+    }
+  }
+`
